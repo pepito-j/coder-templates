8eedc035ad4c9f7b8902410181d7bf03f064d69e
pepzic0les <jullianpepito@gmail.com>
2025-08-10 22:42:36 +0000

    fix(aws/main.tf): Fixes provider reference to point to var.region

--- aws/main.tf
+++ aws/main.tf
@@ -23,7 +23,7 @@ variable "aws_secret_access_key_id" {
 }
 
 provider "aws" {
-    region = "us-east-2"
+    region = var.region
     access_key  = var.aws_access_key_id
     secret_key  = var.aws_secret_access_key_id
 }
